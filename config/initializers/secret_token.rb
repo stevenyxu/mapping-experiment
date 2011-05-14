a6df8b67eea4eaef9f5ab214bc7036dd2c4739da
Steven Xu <steven@stevenxu.ca>
2011-05-14 12:10:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MappingExperiment::Application.config.secret_token = '4212cfa855651a0be3a7e5927b6069b0d57e9811d30779b5f3b3f34d8e2c101f5d2cee54a1daca99ce375916037a8f004e9080d84f15016702c178b472cda17b'
