d66af9156a90c42962d90c67e11af557975138ca
Steven Xu <steven@stevenxu.ca>
2011-05-15 07:12:32 -0400

    changed clip filename to anonymous

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,6 +1,6 @@
 class Clip < ActiveRecord::Base
   belongs_to :sound
   has_attached_file :attachment,
-    :url => '/system/:id/:basename.:extension',
-    :path => ':rails_root/public/system/:id/:basename.:extension'
+    :url => '/system/:id/clip.:extension',
+    :path => ':rails_root/public/system/:id/clip.:extension'
 end
