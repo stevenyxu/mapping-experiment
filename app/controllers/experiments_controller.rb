17068aeb72ff50ae78406f2b6697a719492a1314
Steven Xu <steven@stevenxu.ca>
2011-05-15 16:46:06 -0400

    added redirect to root if experiment complete

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,6 +1,10 @@
 class ExperimentsController < ApplicationController
   def show
     @experiment = Experiment.find(params[:id])
-    redirect_to @experiment.first_incomplete_phase unless @experiment.complete?
+    if @experiment.complete?
+      redirect_to root_path
+    else
+      redirect_to @experiment.first_incomplete_phase 
+    end
   end
 end
